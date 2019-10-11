05c2313dc38da97539dc7a776e59a59640a30345
Shota Itakura <1531860+itkr@users.noreply.github.com>
2019-10-11 11:26:19 +0900

    継承・抽象クラス・インターフェイス

--- classes.ts
+++ classes.ts
@@ -25,3 +25,50 @@ let point = new Point(111, 222);
 console.log(point.x)
 point.forward().forward()
 console.log(point.x)
+
+// 親クラス
+class Js {
+    constructor(private name: string) { }
+    public hello(): string {
+        return 'Hello ' + this.name
+    }
+}
+
+let js = new Js('aaa')
+console.log(js.hello())
+
+// 継承
+class Ts extends Js {
+    // オーバーライド
+    public hello(): string {
+        return super.hello() + ' !'
+    }
+}
+
+let ts = new Ts('aaa')
+console.log(ts.hello())
+
+// 抽象クラス
+abstract class Human {
+    abstract hello(msg: string): string;
+}
+
+class Man extends Human {
+    public hello(msg: string): string {
+        return 'hello ' + msg;
+    }
+}
+
+// インターフェイス
+interface Human2 {
+    name: string;
+    age?: number;  // 任意
+    hello(msg: string): string;
+}
+
+class Mario implements Human2 {
+    constructor(public name: string, public age?: number) { }
+    public hello(msg: string): string {
+        return `${msg} ${this.name}`;
+    }
+}
